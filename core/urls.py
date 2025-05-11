c7bfbd37525683acde444acf395029892eb4d0b8
SafarovSardorDev <sardor.safarov200320@gmail.com>
2025-05-12 03:21:18 +0500

    for deployment

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path
 from bins.views import index, update_bin, get_bins_data, login_view, logout_view
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,3 +12,5 @@ urlpatterns = [
     path('api/update-bin/', update_bin, name='update_bin'),
     path('get-bins-data/', get_bins_data, name='get_bins_data'),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
